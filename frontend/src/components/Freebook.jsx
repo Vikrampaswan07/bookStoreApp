e4bfd684dc62fc762e69d86b257e9788dff868f0
Vikrampaswan07 <130029534+Vikrampaswan07@users.noreply.github.com>
2024-08-12 22:35:41 +0530

    url updated

--- frontend/src/components/Freebook.jsx
+++ frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ export default function Freebook() {
     useEffect(() => {
         const getBook = async () => {
             try {
-                const res = await axios.get("http://localhost:4001/book");
+                const res = await axios.get("https://bookstoreapp-2-vaua.onrender.com/book");
                 
                 const data = res.data.filter((data) => data.category === "Free");
                 console.log(data);
